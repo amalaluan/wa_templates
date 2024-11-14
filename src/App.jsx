40374267fbcd26b880e8bda96b19e054cf4721e0
Arvin Malaluan <arvinmalaluan7@gmail.com>
2024-11-14 22:27:53 +0800

    Template initialization

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-[100dvh] flex items-center flex-col justify-center">
+      <p className="font-semibold">Is classes working?</p>
+      <p className="text-sm">I guess so?</p>
+    </div>
+  );
+}
+
+export default App;
